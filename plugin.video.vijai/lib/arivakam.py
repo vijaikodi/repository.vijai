2cbc31adba7aff7171fb8258adc09a7208290bab
vijaikodi <vijai.djearam@ynadex.com>
2021-09-21 21:13:51 -0700

    corrected arivakam scraper

--- plugin.video.vijai/lib/arivakam.py
+++ plugin.video.vijai/lib/arivakam.py
@@ -1,4 +1,5 @@
 import urllib.request, urllib.error, urllib.parse,re,xbmc,urllib.request,urllib.parse,urllib.error,requests
+from urllib.parse import urlparse
 from .unpack import unpack
 def getdatacontent_dict(url,reg):
     proxy_handler = urllib.request.ProxyHandler({})
@@ -32,6 +33,19 @@ def resolve_arivakam(url):
         link1 = getdatacontent(url,reg)
         link1 = link1[0]
         link = getredirectedurl(link1)
+        link = link.split("?")
+        link = link[0]
+        t1 = urlparse(link)
+        t2 = t1.path
+        r = t1.path.split("/")[-2].split(",")[-1]
+        t3 = t2.split("/")
+        t3[-2] = r
+        r = "/".join(t3)
+        t=t1._replace(path=r)
+        link = t.geturl()
+        xbmc.log("----------------------------------------Arivakam----------------------------------------------------------------")
+        xbmc.log(link)
+        #['https://32vod-adaptive.akamaized.net/exp=1631566344~acl=%2Fd5994a51-6d0f-410c-8cab-f0e2b55704af%2F%2A~hmac=9ba5e07383d402bd8f10f4692eae6f169647905e44541f7e1f4d5b3d6aa9c5f6/d5994a51-6d0f-410c-8cab-f0e2b55704af/sep/video/589c9043,281f15ba,279905cd,43a500be/master.m3u8', 'absolute=1']
         return link
     except:
         return None    
