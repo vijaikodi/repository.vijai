aa6ae9cfed82c70e36530198e2875668e40b91c9
vijaikodi <vijai.djearam@yandex.com>
2022-10-09 01:59:57 -0700

    fixed arivakam resolver

--- plugin.video.vijai/lib/arivakam.py
+++ plugin.video.vijai/lib/arivakam.py
@@ -28,6 +28,7 @@ def getdatacontent(url,reg):
     html = resp.text
     xbmc.log(html)
     data = re.compile(reg).findall(html)
+    xbmc.log(str(data))
     return data
 def getredirectedurl(url):
     try:
@@ -52,8 +53,8 @@ def getredirectedurl(url):
 #         r = "/".join(t3)
 #         t=t1._replace(path=r)
 #         link = t.geturl()
-#         xbmc.log("----------------------------------------Arivakam----------------------------------------------------------------")
-#         xbmc.log(link)
+#         #xbmc.log("----------------------------------------Arivakam----------------------------------------------------------------")
+#         #xbmc.log(link)
 #         #['https://32vod-adaptive.akamaized.net/exp=1631566344~acl=%2Fd5994a51-6d0f-410c-8cab-f0e2b55704af%2F%2A~hmac=9ba5e07383d402bd8f10f4692eae6f169647905e44541f7e1f4d5b3d6aa9c5f6/d5994a51-6d0f-410c-8cab-f0e2b55704af/sep/video/589c9043,281f15ba,279905cd,43a500be/master.m3u8', 'absolute=1']
 #         return link
 #     except:
@@ -82,31 +83,51 @@ def resolve_arivakam(url):
         xbmc.log(str(data))
         for url in data:
             if 'vimeo' in url:
-                xbmc.log(url)
+                #xbmc.log(url)
                 headers = {
-                    'authority': 'play.arivakam.net',
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                    'accept-language': 'en-US,en;q=0.9',
-                    'referer': 'https://tamilgun.asia/',
-                    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                    'Accept-Language': 'en-US,en;q=0.9,ta-IN;q=0.8,ta;q=0.7,fr-FR;q=0.6,fr;q=0.5',
+                    'Connection': 'keep-alive',
+                    'DNT': '1',
+                    'Referer': 'https://play.arivakam.net/',
+                    'Sec-Fetch-Dest': 'iframe',
+                    'Sec-Fetch-Mode': 'navigate',
+                    'Sec-Fetch-Site': 'cross-site',
+                    'Upgrade-Insecure-Requests': '1',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+                    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
                     'sec-ch-ua-mobile': '?0',
                     'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'iframe',
-                    'sec-fetch-mode': 'navigate',
-                    'sec-fetch-site': 'cross-site',
-                    'upgrade-insecure-requests': '1',
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36',
                 }
-                response = requests.get(url, headers=headers,allow_redirects=False)
-                url = response.headers['Location']
-                xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
-                xbmc.log(url)
+                #response = requests.get(url, headers=headers)
+                reg = "\"avc_url\":\"(.*?)\""
+                url = getdatacontent(url,reg)
+                #url = re.compile(reg).findall(response)
+                #xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
+                ##xbmc.log(str(response))
+                #xbmc.log(str(url))
                 if url:
+                    url = url[2]
                     return url
                 else:
                     return None
-            if url:
-                return url
+            if 'cdntest.diebutx' in url:
+                if url:
+                    t = url.split('playlist.m3u8')
+                    start_url = t[0]
+                    xbmc.log(start_url)
+                    reg = "NAME=720p\s+(.*?)\s+#EXT"
+                    end_url = getdatacontent(url,reg)
+                    if end_url:
+                        end_url = end_url[0]
+                        xbmc.log(end_url)
+                        url = start_url+end_url
+                        xbmc.log(url)
+                        return url
+                    else:
+                        return None
+                else:
+                    return None
             else:
                 return None
     except:
@@ -117,19 +138,19 @@ def resolve_arivakam(url):
     # url = url[1]
     # url = url.rstrip(url[-1])
     # turl ='https://play.arivakam.net/player/api_player.php?id='+url+'&type=hls&key=1'
-    # xbmc.log('--------------------------------------Entering Arivakam-----------------------------------------------------')
-    # xbmc.log(turl)
+    # #xbmc.log('--------------------------------------Entering Arivakam-----------------------------------------------------')
+    # #xbmc.log(turl)
     # reg = 'eval(.*?)\s+<'
     # resp = getdatacontent(turl,reg)
     # if resp:
     #     data = unpack(resp[0])
     #     reg = '"file":\"(.*?)\"'
     #     data = re.compile(reg).findall(data)
-    #     xbmc.log('--------------------------------------Entering Arivakam DATA Key = 1-----------------------------------------------------')
-    #     xbmc.log(str(data))
+    #     #xbmc.log('--------------------------------------Entering Arivakam DATA Key = 1-----------------------------------------------------')
+    #     #xbmc.log(str(data))
     #     url = data[0]
     #     if 'vimeo' in url:
-    #         xbmc.log(url)
+    #         #xbmc.log(url)
     #         headers = {
     #         'Accept': '*/*',
     #         'Accept-Language': 'en-US,en;q=0.9',
@@ -146,15 +167,15 @@ def resolve_arivakam(url):
     #         }
     #         response = requests.get(url, headers=headers,allow_redirects=False)
     #         url = response.headers['Location']
-    #         xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
-    #         xbmc.log(url)
+    #         #xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
+    #         #xbmc.log(url)
     #         if url:
     #             return url
     #         else:
     #             return None
     #     elif 'playallu' in url:
-    #         xbmc.log('------------------------------------------ENtering Playallu------------------------------------------------------------------')
-    #         xbmc.log(url)
+    #         #xbmc.log('------------------------------------------ENtering Playallu------------------------------------------------------------------')
+    #         #xbmc.log(url)
     #         movieurl = playallu.resolve_playallu(url)
     #         if movieurl:
     #             return url
@@ -162,8 +183,8 @@ def resolve_arivakam(url):
     #             return None
     # else:
     #     turl ='https://play.arivakam.net/player/api_player.php?id='+url+'&type=hls&key=0'
-    #     xbmc.log('--------------------------------------Entering Arivakam DATA Key = 0-----------------------------------------------------')
-    #     xbmc.log(turl)
+    #     #xbmc.log('--------------------------------------Entering Arivakam DATA Key = 0-----------------------------------------------------')
+    #     #xbmc.log(turl)
     #     reg = 'eval(.*?)\s+<'
     #     resp = getdatacontent(turl,reg)
     #     if resp:
@@ -172,7 +193,7 @@ def resolve_arivakam(url):
     #         data = re.compile(reg).findall(data)
     #         url = data[0]
     #         if 'vimeo' in url:
-    #             xbmc.log(url)
+    #             #xbmc.log(url)
     #             headers = {
     #             'Accept': '*/*',
     #             'Accept-Language': 'en-US,en;q=0.9',
@@ -189,15 +210,15 @@ def resolve_arivakam(url):
     #             }
     #             response = requests.get(url, headers=headers,allow_redirects=False)
     #             url = response.headers['Location']
-    #             xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
-    #             xbmc.log(url)
+    #             #xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
+    #             #xbmc.log(url)
     #             if url:
     #                 return url
     #             else:
     #                 return None
     #         elif 'playallu' in url:
-    #             xbmc.log('------------------------------------------ENtering Playallu------------------------------------------------------------------')
-    #             xbmc.log(url)
+    #             #xbmc.log('------------------------------------------ENtering Playallu------------------------------------------------------------------')
+    #             #xbmc.log(url)
     #             movieurl = playallu.resolve_playallu(url)
     #             if movieurl:
     #                 return url
@@ -207,19 +228,19 @@ def resolve_arivakam(url):
 
     # try:
     #         turl ='https://play.arivakam.net/player/api_player.php?id='+url+'&type=hls&key=1'
-    #         xbmc.log('--------------------------------------Entering Arivakam-----------------------------------------------------')
-    #         xbmc.log(turl)
+    #         #xbmc.log('--------------------------------------Entering Arivakam-----------------------------------------------------')
+    #         #xbmc.log(turl)
     #         reg = 'eval(.*?)\s+<'
     #         resp = getdatacontent(turl,reg)
     #         data = unpack(resp[0])
     #         reg = '"file":\"(.*?)\"'
     #         data = re.compile(reg).findall(data)
     #         if data[0]:
-    #             xbmc.log('--------------------------------------Entering Arivakam DATA Key = 1-----------------------------------------------------')
-    #             xbmc.log(str(data))
+    #             #xbmc.log('--------------------------------------Entering Arivakam DATA Key = 1-----------------------------------------------------')
+    #             #xbmc.log(str(data))
     #             url = data[0]
     #             if 'vimeo' in url:
-    #                 xbmc.log(url)
+    #                 #xbmc.log(url)
     #                 headers = {
     #                 'Accept': '*/*',
     #                 'Accept-Language': 'en-US,en;q=0.9',
@@ -236,15 +257,15 @@ def resolve_arivakam(url):
     #                 }
     #                 response = requests.get(url, headers=headers,allow_redirects=False)
     #                 url = response.headers['Location']
-    #                 xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
-    #                 xbmc.log(url)
+    #                 #xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
+    #                 #xbmc.log(url)
     #                 if url:
     #                     return url
     #                 else:
     #                     return None
     #             elif 'playallu' in url:
-    #                 xbmc.log('------------------------------------------ENtering Playallu------------------------------------------------------------------')
-    #                 xbmc.log(url)
+    #                 #xbmc.log('------------------------------------------ENtering Playallu------------------------------------------------------------------')
+    #                 #xbmc.log(url)
     #                 movieurl = playallu.resolve_playallu(url)
     #                 if movieurl:
     #                     return url
@@ -252,8 +273,8 @@ def resolve_arivakam(url):
     #                     return None
     #         else:
     #             turl ='https://play.arivakam.net/player/api_player.php?id='+url+'&type=hls&key=0'
-    #             xbmc.log('--------------------------------------Entering Arivakam DATA Key = 0-----------------------------------------------------')
-    #             xbmc.log(turl)
+    #             #xbmc.log('--------------------------------------Entering Arivakam DATA Key = 0-----------------------------------------------------')
+    #             #xbmc.log(turl)
     #             reg = 'eval(.*?)\s+<'
     #             resp = getdatacontent(turl,reg)
     #             data = unpack(resp[0])
@@ -262,7 +283,7 @@ def resolve_arivakam(url):
     #             if data[0]:
     #                 url = data[0]
     #                 if 'vimeo' in url:
-    #                     xbmc.log(url)
+    #                     #xbmc.log(url)
     #                     headers = {
     #                     'Accept': '*/*',
     #                     'Accept-Language': 'en-US,en;q=0.9',
@@ -279,15 +300,15 @@ def resolve_arivakam(url):
     #                     }
     #                     response = requests.get(url, headers=headers,allow_redirects=False)
     #                     url = response.headers['Location']
-    #                     xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
-    #                     xbmc.log(url)
+    #                     #xbmc.log('------------------------------------------ENtering Arivakam------------------------------------------------------------------')
+    #                     #xbmc.log(url)
     #                     if url:
     #                         return url
     #                     else:
     #                         return None
     #                 elif 'playallu' in url:
-    #                     xbmc.log('------------------------------------------ENtering Playallu------------------------------------------------------------------')
-    #                     xbmc.log(url)
+    #                     #xbmc.log('------------------------------------------ENtering Playallu------------------------------------------------------------------')
+    #                     #xbmc.log(url)
     #                     movieurl = playallu.resolve_playallu(url)
     #                     if movieurl:
     #                         return url
